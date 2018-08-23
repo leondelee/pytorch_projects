f29ecae2c143c68dc7626f3ccfdaa867e420bf80
speit <liliangwei@sjtu.edu.cn>
2018-08-23 15:15:47 +0800

    Add some comments to config.py

--- cat_dog/config.py
+++ cat_dog/config.py
@@ -11,13 +11,8 @@ class Configuration:
     model_name = 'AlexNet'  # the name of model to be used, which has to be same as the one in 'models/__init__.py'
     batch_size = 1        # number of batches
     load_data_workers = 1   # number of workers for data loading
-    train_all_path = '/home/speit/torch/data/cat_dog/training_set/all/'
-    test_all_path = '/home/speit/torch/data/cat_dog//test_set/all/'
-    train_dog_path = '/home/speit/torch/cat_dog/data_related/training_set/dogs/'
-    #train_dog_path = '/home/speit/torch/cat_dog/data_related/demo_data/'
-    train_cat_path = '/home/speit/torch/cat_dog/data_related/training_set/cats/'
-    test_dog_path = '/home/speit/torch/cat_dog/data_related/test_set/dogs/'
-    test_cat_path = '/home/speit/torch/cat_dog/data_related/test_set/cats/'
+    train_all_path = '/home/speit/torch/data/cat_dog/training_set/all/' # the path to training data
+    test_all_path = '/home/speit/torch/data/cat_dog//test_set/all/'     # the path to test data
     use_gpu = False         # whether to use gpu or not
     print_frequency = 20    # print info every print_frequency batches
     result_file = '/result/result.csv'  # the position to store result
@@ -25,11 +20,16 @@ class Configuration:
     learning_rate = 0.01    # learning rate
     learning_decay = 0.95   # when loss value increase, learning_rate = learning_decay * learning_rate
     weight_decay = 1e-4
-    time_format = "%m_%d_%H:%M:%S"
-    log_dir_path = '/home/speit/torch/cat_dog/log/'
-    checkpoint_dir_path = '/home/speit/torch/checkpoints/cat_dog/'
+    time_format = "%m_%d_%H:%M:%S"  # time format used in this project for logging and saving model
+    log_dir_path = '/home/speit/torch/cat_dog/log/' # the path to log folder
+    checkpoint_dir_path = '/home/speit/torch/checkpoints/cat_dog/' # the path to store trained model
 
     def parse(self, kwargs):
+        """
+        add additional attributes to Configuration class
+        :param kwargs:
+        :return:
+        """
         import warnings
         for key, value in kwargs.items():
             if not hasattr(self, key):
